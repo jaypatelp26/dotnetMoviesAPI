5941b4d3cf71fb650aa3f4288e1c0f8c42f09c07
Jay Patel <jaypatelp26@gmail.com>
2024-11-15 03:48:24 -0500

    Add .gitignore and initial project files

--- obj/Debug/net8.0/MoviesAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/MoviesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0505334f45c45afb700be0fbc5e4d481522d1b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
